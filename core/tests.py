1da402974d1189a7ef85e6e804b98bb42ac4c8f1
Michele Urbani <michele@didatec.com>
2021-04-29 17:07:34 +0200

    Tests for AAKR methods rewritten.

--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,8 @@
 
 import os
-import numpy as np
 import unittest
-from random import choice, seed
+import numpy as np
+from random import choice, seed, sample
 from itertools import chain
 from core.data_handler import (
     read,
@@ -65,42 +65,74 @@ class TestAAKR(unittest.TestCase):
 
     def setUp(self):
         self.X = load_training_dataset(percent_data=0.2)
+        # Select a random dataset containing failures
+        self.Y = read_dataset(
+            os.path.join(
+                "training_validation_2",
+                *sample(os.listdir("training_validation_2"), 1)
+            )
+        )
         self.aakr = AAKR()
 
+    def test_fit(self):
+        self.aakr.fit(self.X, self.Y)
+        self.assertIsNotNone(self.aakr.pipe)
+        self.assertIsNotNone(self.aakr.features)
+
+    def test_transform(self):
+        self.aakr.fit(self.X, self.Y)
+        X, Y = self.aakr.transform(self.X, self.Y)
+        self.assertTrue(type(X) is np.ndarray)
+        self.assertTrue(type(Y) is np.ndarray)
+        self.assertEqual(self.X.shape[0], len(X))
+        self.assertEqual(self.Y.shape[0], len(Y))
+        self.assertGreaterEqual(self.X.shape[1], X.shape[1])
+        self.assertGreaterEqual(self.Y.shape[1], Y.shape[1])
+
     def test_predict(self):
-        Y = read_dataset("training_validation_2/class_0_101_data.csv")
-        Y = self.aakr.predict(self.X, Y)
-        # Check that the number of features is the same for the train and test
-        # sets.
-        X = self.aakr.transform(self.X)
-        self.assertEqual(X.shape[1], Y.shape[1])
+        Y, Y_hat = self.aakr.predict(self.X, self.Y)
+        self.assertIsNotNone(self.aakr.VI)
 
-    def test_features_retrieval(self):
-        Y = read_dataset("training_validation_2/class_0_101_data.csv")
-        Y = self.aakr.fit(Y)
-        mask = self.aakr.pipe.named_steps["variancethreshold"].get_support()
-        self.assertEqual(len(mask), Y.shape[1])
+#     def test_features_retrieval(self):
+#         Y = read_dataset("training_validation_2/class_0_101_data.csv")
+#         Y = self.aakr.fit(Y)
+#         mask = self.aakr.pipe.named_steps["variancethreshold"].get_support()
+#         self.assertEqual(len(mask), Y.shape[1])
 
 
-class TestModifiedAAKR(unittest.TestCase):
+# class TestModifiedAAKR(unittest.TestCase):
 
-    def setUp(self):
-        self.X = load_training_dataset(percent_data=0.2)
-        self.aakr = ModifiedAAKR()
-        self.Y = read_dataset("training_validation_2/class_0_101_data.csv")
+#     def setUp(self):
+#         self.X = load_training_dataset(percent_data=0.2)
+#         self.aakr = ModifiedAAKR()
+#         self.Y = read_dataset("training_validation_2/class_0_101_data.csv")
 
-    def test_abs_normalized_distance(self):
-        X, Y = self.aakr.fit_transform(self.X, self.Y)
-        dist_shape = self.aakr.abs_normalized_distance(X, Y).shape
-        self.assertEqual(
-            dist_shape,
-            (Y.shape[0], self.X.shape[0], Y.shape[1])
-        )
+#     def test_abs_normalized_distance(self):
+#         X, Y = self.aakr.fit_transform(self.X, self.Y)
+#         dist_shape = self.aakr.abs_normalized_distance(X, Y).shape
+#         self.assertEqual(
+#             dist_shape,
+#             (Y.shape[0], self.X.shape[0], Y.shape[1])
+#         )
 
-    def test_permutation_matrix(self):
-        X, Y = self.aakr.fit_transform(self.X, self.Y)
-        rnd_obs = Y[np.random.randint(Y.shape[0]), :]
-        dist = self.aakr.abs_normalized_distance(X, rnd_obs)
-        P = self.aakr.permutation_matrix(dist)
-        p_rnd_obs = np.dot(rnd_obs, P)
-        self.assertGreaterEqual(p_rnd_obs[1], p_rnd_obs[-1])
+#     def test_permutation_matrix(self):
+#         X, Y = self.aakr.fit_transform(self.X, self.Y)
+#         rnd_obs = Y[np.random.randint(Y.shape[0]), :]
+#         dist = self.aakr.abs_normalized_distance(X, rnd_obs)
+#         P = self.aakr.permutation_matrix(dist)
+#         p_rnd_obs = np.dot(rnd_obs, P)
+#         self.assertGreaterEqual(p_rnd_obs[1], p_rnd_obs[-1])
+
+
+# class TestTraining(unittest.TestCase):
+
+#     def setUp(self):
+#         self.X = load_training_dataset(percent_data=0.2)
+#         self.aakr = ModifiedAAKR
+#         self.Y = read_dataset("training_validation_2/class_0_101_data.csv")
+
+#     def test_training(self):
+#         cross_validation(
+#             classifier=self.aakr,
+#             data=self.X,
+#         )
